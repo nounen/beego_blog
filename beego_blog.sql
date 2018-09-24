461122759c5d3ee5a261ea3351925db77fb5dd2a
nounen <1394139539@qq.com>
2018-09-24 22:36:25 +0800

    update: sql file

--- beego_blog.sql
+++ beego_blog.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50723
  File Encoding         : 65001
 
- Date: 17/09/2018 00:21:19
+ Date: 24/09/2018 22:36:04
 */
 
 SET NAMES utf8mb4;
@@ -23,14 +23,56 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `article`;
 CREATE TABLE `article`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `status` tinyint(4) NULL DEFAULT 1,
-  `title` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `content` longtext CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
+  `cover` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `state` tinyint(4) NULL DEFAULT 1,
   `user_id` int(11) NULL DEFAULT NULL,
   `created_at` datetime(0) NULL DEFAULT NULL,
   `deleted_at` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of article
+-- ----------------------------
+INSERT INTO `article` VALUES (1, 'https://odu38kv7q.qnssl.com/nice.png', 'title1', 'content1', 1, 1, '2018-09-22 10:01:46', NULL);
+INSERT INTO `article` VALUES (2, 'https://odu38kv7q.qnssl.com/nice.png', 'title2', 'content', 2, 1, '2018-09-22 10:04:22', NULL);
+
+-- ----------------------------
+-- Table structure for article_tag
+-- ----------------------------
+DROP TABLE IF EXISTS `article_tag`;
+CREATE TABLE `article_tag`  (
+  `article_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of article_tag
+-- ----------------------------
+INSERT INTO `article_tag` VALUES (1, 1);
+INSERT INTO `article_tag` VALUES (1, 2);
+
+-- ----------------------------
+-- Table structure for comment
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` datetime(0) NULL DEFAULT NULL,
+  `deleted_at` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
+INSERT INTO `comment` VALUES (1, 1, 'tour1', 'nice', '2018-09-22 13:58:50', NULL);
+INSERT INTO `comment` VALUES (2, 1, 'tour2', 'good!', '2018-09-22 14:00:02', NULL);
 
 -- ----------------------------
 -- Table structure for tag
@@ -38,11 +80,47 @@ CREATE TABLE `article`  (
 DROP TABLE IF EXISTS `tag`;
 CREATE TABLE `tag`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `created_at` datetime(0) NULL DEFAULT NULL,
   `deleted_at` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `name_index`(`name`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 54 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 64 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tag
+-- ----------------------------
+INSERT INTO `tag` VALUES (1, 'tag1', '2018-09-24 18:15:58', NULL);
+INSERT INTO `tag` VALUES (2, 'Tag2', '2018-09-15 22:47:24', NULL);
+INSERT INTO `tag` VALUES (3, 'Tag3', '2018-09-15 23:11:17', NULL);
+INSERT INTO `tag` VALUES (4, 'Tag4', '2018-09-15 23:12:45', NULL);
+INSERT INTO `tag` VALUES (54, 'Tag5', '2018-09-22 16:29:03', NULL);
+INSERT INTO `tag` VALUES (56, 'Tag55', '2018-09-24 17:00:06', NULL);
+INSERT INTO `tag` VALUES (57, 'Tag56', '2018-09-24 17:09:58', NULL);
+INSERT INTO `tag` VALUES (58, 'Tag57', '2018-09-24 17:25:02', NULL);
+INSERT INTO `tag` VALUES (59, 'Tag58', '2018-09-24 17:25:42', NULL);
+INSERT INTO `tag` VALUES (61, 'Tag59', '2018-09-24 17:26:54', NULL);
+INSERT INTO `tag` VALUES (62, 'Tag60', '2018-09-24 17:27:22', NULL);
+INSERT INTO `tag` VALUES (63, 'Tag61', '2018-09-24 17:47:31', NULL);
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `state` tinyint(4) NOT NULL,
+  `created_at` datetime(0) NULL DEFAULT NULL,
+  `deleted_at` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `user_name_uindex`(`name`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'admin', '111111', 1, '2018-09-22 09:12:57', NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
